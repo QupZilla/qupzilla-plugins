24a3531efbe917b336fd45025d04a75d4df4b20c
Jonathan Hooverman <Jonathan.Hooverman@gmail.com>
2012-04-21 17:55:18 +0200

    Added German translation of YouTuber plugin

---
+++ YouTuber/translations/de_DE.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
+<context>
+    <name>YouTuber_Handler</name>
+    <message>
+        <source>View via YouTuber</source>
+        <translation>Mit YouTuber ansehen</translation>
+    </message>
+    <message>
+        <source>Executable: </source>
+        <translation>Ausführbare Datei: </translation>
+    </message>
+    <message>
+        <source>Arguments: </source>
+        <translation>Parameter: </translation>
+    </message>
+    <message>
+        <source>Cannot start external viewer</source>
+        <translation>Externer Player kann nicht gestartet werden</translation>
+    </message>
+    <message>
+        <source>Cannot start external viewer! %1</source>
+        <translation>Externer Player kann nicht gestartet werden! %1</translation>
+    </message>
+</context>
+<context>
+    <name>YouTuber_Settings</name>
+    <message>
+        <source>Executable:</source>
+        <translation>Ausführbare Datei: </translation>
+    </message>
+    <message>
+        <source>Arguments:</source>
+        <translation>Parameter: </translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <source>&lt;h2&gt;External YouTube video viewer&lt;/h2&gt;</source>
+        <translation>&lt;h2&gt;Externer YouTube Video Player&lt;/h2&gt;</translation>
+    </message>
+    <message>
+        <source>YouTuber Settings</source>
+        <translation>YouTuber Einstellungen</translation>
+    </message>
+</context>
+</TS>
