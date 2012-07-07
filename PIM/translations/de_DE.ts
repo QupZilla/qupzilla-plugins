57b8e19e79236c89cedd9eca7495b120c977843c
Jonathan Hooverman <Jonathan.Hooverman@gmail.com>
2012-07-07 14:12:18 +0200

    Added German translation for PIM plugin

---
+++ PIM/translations/de_DE.ts
@@ -0,0 +1,175 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE" sourcelanguage="en_US">
+<context>
+    <name>PIM_Handler</name>
+    <message>
+        <location filename="../PIM_handler.cpp" line="60"/>
+        <source>First Name</source>
+        <translation>Vorname</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="59"/>
+        <source>Last Name</source>
+        <translation>Nachname</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="61"/>
+        <source>E-mail</source>
+        <translation>E-Mail</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="63"/>
+        <source>Phone</source>
+        <translation>Telefon</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="62"/>
+        <source>Mobile</source>
+        <translation>Mobiltelefon</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="64"/>
+        <source>Address</source>
+        <translation>Adresse</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="65"/>
+        <source>City</source>
+        <translation>Stadt</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="66"/>
+        <source>Zip</source>
+        <translation>Postleitzahl</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="67"/>
+        <source>State/Region</source>
+        <translation>Bundesland/ Region</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="68"/>
+        <source>Country</source>
+        <translation>Land</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="69"/>
+        <source>Home Page</source>
+        <translation>Homepage</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="70"/>
+        <source>Special 1</source>
+        <translation>Weitere 1</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="71"/>
+        <source>Special 2</source>
+        <translation>Weitere 2</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="72"/>
+        <source>Special 3</source>
+        <translation>Weitere 3</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="110"/>
+        <source>Insert Personal</source>
+        <translation>Bild einfügen</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="124"/>
+        <source>Edit</source>
+        <translation>Bearbeiten</translation>
+    </message>
+</context>
+<context>
+    <name>PIM_Settings</name>
+    <message>
+        <location filename="../PIM_settings.ui" line="14"/>
+        <source>PIM Settings</source>
+        <translation>PIM Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="58"/>
+        <source>First Name:</source>
+        <translation>Vorname:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="68"/>
+        <source>Last Name:</source>
+        <translation>Nachname:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="78"/>
+        <source>E-Mail:</source>
+        <translation>E-Mail:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="88"/>
+        <source>Phone:</source>
+        <translation>Telefon:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="98"/>
+        <source>Mobile Phone:</source>
+        <translation>Mobiltelefon:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="108"/>
+        <source>Address:</source>
+        <translation>Adresse:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="118"/>
+        <source>City:</source>
+        <translation>Stadt:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="128"/>
+        <source>ZIP Code:</source>
+        <translation>Postleitzahl:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="138"/>
+        <source>State/Region:</source>
+        <translation>Bundesland/ Region:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="148"/>
+        <source>Country:</source>
+        <translation>Land:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="158"/>
+        <source>Home Page:</source>
+        <translation>Homepage:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="168"/>
+        <source>Custom 1:</source>
+        <translation>Benutzerdefiniert 1:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="178"/>
+        <source>Custom 2:</source>
+        <translation>Benutzerdefiniert 2:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="188"/>
+        <source>Custom 3:</source>
+        <translation>Benutzerdefiniert 3:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="226"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:x-large; font-weight:600;&quot;&gt;Personal Information Manager&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:x-large; font-weight:600;&quot;&gt;Personal Information Manager&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="262"/>
+        <source>&lt;b&gt;Note:&lt;/b&gt; Press Ctrl+ENTER to autofill form fields for which QupZilla finds personal entries.</source>
+        <translation>&lt;b&gt;Note:&lt;/b&gt; Drücken Sie STRG+ENTER um die Formularfelder automatisch ausfüllen zu lassen.</translation>
+    </message>
+</context>
+</TS>
