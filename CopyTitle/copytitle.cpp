6db80f7b91612189b62c70a131ced4a283e020ce
nowrep <nowrep@gmail.com>
2013-05-25 00:02:17 +0200

    Let's require latest development version (1.5.0)

--- CopyTitle/copytitle.cpp
+++ CopyTitle/copytitle.cpp
@@ -61,7 +61,7 @@ void CopyTitle::unload()
 
 bool CopyTitle::testPlugin()
 {
-    return (QupZilla::VERSION == QLatin1String("1.4.0"));
+    return (QupZilla::VERSION == QLatin1String("1.5.0"));
 }
 
 QTranslator* CopyTitle::getTranslator(const QString &locale)
