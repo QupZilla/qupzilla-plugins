623417db5cc26c3109210d89f6710733b8dbaab1
Mladen Pejaković <pejakm@gmail.com>
2013-06-07 21:17:31 +0200

    Add empty translation file

---
+++ CopyTitle/translations/empty.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>CopyTitle</name>
+    <message>
+        <source>Copy Page Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copy Image Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ MailHandle/translations/empty.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>MailHandle_Settings</name>
+    <message>
+        <source>MailHandle Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:x-large; font-weight:600;&quot;&gt;Mailto links handler&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Web service to use:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Gmail</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mail.ru</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <source>Яandex</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Outlook</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Yahoo!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>My Opera</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>FastMail</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>T-Online</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
