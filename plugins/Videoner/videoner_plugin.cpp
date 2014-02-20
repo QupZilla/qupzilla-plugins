c58e85a7453bdaeb02b4def6ed1706b230ed86dc
Mladen Pejaković <pejakm@autistici.org>
2014-02-20 13:36:11 +0100

    [Plugins] Update for 1.6.3

--- plugins/Videoner/videoner_plugin.cpp
+++ plugins/Videoner/videoner_plugin.cpp
@@ -62,7 +62,7 @@ bool Videoner_Plugin::testPlugin()
 {
     // Let's be sure, require latest version of QupZilla
 
-    return (QupZilla::VERSION == QLatin1String("1.6.1"));
+    return (QupZilla::VERSION == QLatin1String("1.6.3"));
 }
 
 QTranslator* Videoner_Plugin::getTranslator(const QString &locale)
