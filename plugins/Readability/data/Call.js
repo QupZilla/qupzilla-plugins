8174995c69d0b14f51888c61c9eb01e28b3652a4
Jaroslav Bambas <jbambas@seznam.cz>
2016-04-24 10:32:19 +0200

    Incorporation of comments from pull request.

--- plugins/Readability/data/Call.js
+++ plugins/Readability/data/Call.js
@@ -3,31 +3,31 @@
 %2
 
 function callReadability(){
-	var loc = document.location;
-	var uri = {
-  		spec: loc.href,
-  		host: loc.host,
-  		prePath: loc.protocol + "//" + loc.host,
-  		scheme: loc.protocol.substr(0, loc.protocol.indexOf(":")),
-  		pathBase: loc.protocol + "//" + loc.host + loc.pathname.substr(0, loc.pathname.lastIndexOf("/") + 1)
-	};
-	var article = new Readability(uri, document).parse();
-	renderPage(article);
+    var loc = document.location;
+    var uri = {
+        spec: loc.href,
+        host: loc.host,
+        prePath: loc.protocol + "//" + loc.host,
+        scheme: loc.protocol.substr(0, loc.protocol.indexOf(":")),
+        pathBase: loc.protocol + "//" + loc.host + loc.pathname.substr(0, loc.pathname.lastIndexOf("/") + 1)
+    };
+    var article = new Readability(uri, document).parse();
+    renderPage(article);
 }
 
 function readabilityHead(article){
-	var head = document.createElement('head');
-	document.documentElement.appendChild(head);
+    var head = document.createElement('head');
+    document.documentElement.appendChild(head);
 
-	var title = document.createElement('title');
-	title.innerHTML = article.title;
-	head.appendChild(title);
+    var title = document.createElement('title');
+    title.innerHTML = article.title;
+    head.appendChild(title);
 
     var css = document.createElement('style');
     css.innerHTML = '%3';
-	head.appendChild(css);
+    head.appendChild(css);
 
-   	var meta  = document.createElement('meta');
+    var meta  = document.createElement('meta');
     meta.content = "text/html; charset=UTF-8";
     meta.httpEquiv = 'content-type';
     head.appendChild(meta);
@@ -38,63 +38,63 @@ function readabilityHead(article){
 }
 
 function readablilityBody(article){
-	var body = document.createElement('body');
-	body.className = 'loaded sans-serif sepia';
-	document.documentElement.appendChild(body);
+    var body = document.createElement('body');
+    body.className = 'loaded sans-serif sepia';
+    document.documentElement.appendChild(body);
 
-	var container = document.createElement('div');
-	container.className = 'container font-size5';
-	container.id = 'container';
-	body.appendChild(container);
+    var container = document.createElement('div');
+    container.className = 'container font-size5';
+    container.id = 'container';
+    body.appendChild(container);
 
-	var header = document.createElement('div');
-	header.className = 'header';
-	header.id = 'reader-header';
-	header.style.display = 'block';
-	container.appendChild(header);
+    var header = document.createElement('div');
+    header.className = 'header';
+    header.id = 'reader-header';
+    header.style.display = 'block';
+    container.appendChild(header);
 
-	var domain = document.createElement('a');
-	domain.id = 'reader-domain';
-	domain.className = 'domain';
-	domain.href = article.uri.spec;
-	domain.innerHTML = article.uri.host;
-	header.appendChild(domain);
+    var domain = document.createElement('a');
+    domain.id = 'reader-domain';
+    domain.className = 'domain';
+    domain.href = article.uri.spec;
+    domain.innerHTML = article.uri.host;
+    header.appendChild(domain);
 
-	var h1 = document.createElement('h1');
-	h1.id = 'reader-title';
-	h1.innerHTML = article.title;
-	header.appendChild(h1);
+    var h1 = document.createElement('h1');
+    h1.id = 'reader-title';
+    h1.innerHTML = article.title;
+    header.appendChild(h1);
 
-	var credit = document.createElement('div');
-	credit.id = 'reader-credits';
-	credit.className = 'credits';
-	credit.innerHTML = article.byline;
-	header.appendChild(credit);
+    var credit = document.createElement('div');
+    credit.id = 'reader-credits';
+    credit.className = 'credits';
+    credit.innerHTML = article.byline;
+    header.appendChild(credit);
 
-	var content = document.createElement('div');
-	content.className = 'content';
-	container.appendChild(content);
+    var content = document.createElement('div');
+    content.className = 'content';
+    container.appendChild(content);
 
-	var reader_content = document.createElement('div');
-	reader_content.id = 'moz-reader-content';
-	reader_content.style.display = 'block';
-	reader_content.innerHTML = article.content;
-	content.appendChild(reader_content);
+    var reader_content = document.createElement('div');
+    reader_content.id = 'moz-reader-content';
+    reader_content.style.display = 'block';
+    reader_content.innerHTML = article.content;
+    content.appendChild(reader_content);
 }
 
 function renderPage(article){
-	var element = document.documentElement;
-	if(element && !!article && !!article.content){
-		element.removeChild(document.body);
-		element.removeChild(document.head);
+    var element = document.documentElement;
+    if(element && !!article && !!article.content){
+        element.removeChild(document.body);
+        element.removeChild(document.head);
 
-		readabilityHead(article);
-		readablilityBody(article);
+        readabilityHead(article);
+        readablilityBody(article);
 
-		readablilityToolbar();
-	} else {
-		alert("No content to transform.");
-	}
+        readablilityToolbar();
+    } else {
+        alert("No content to transform.");
+    }
 }
 
 callReadability();
